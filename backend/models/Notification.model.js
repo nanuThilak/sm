3b5e4b3609ae678c8b68ecfb422eb6e440776532
nanuThilak <nanuthilak2@gmail.com>
2024-09-21 19:08:21 +0530

    backend complete

--- backend/models/Notification.model.js
+++ backend/models/Notification.model.js
@@ -4,12 +4,12 @@ const NotificationSchema = new mongoose.Schema(
   {
     from: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
+      ref: "Users",
       required: true,
     },
     to: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
+      ref: "Users",
       required: true,
     },
     type: {
